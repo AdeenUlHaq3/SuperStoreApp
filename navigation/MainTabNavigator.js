371c2e21da6004a905a871b02668f0241d8a665d
adeenulhaq3 <adeen553@gmail.com>
2018-12-13 23:45:52 +0500

    Foldview

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -3,7 +3,8 @@ import { Platform } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
 import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/User';
+// import HomeScreen from '../screens/User';
+import HomeScreen from '../ExampleList';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
